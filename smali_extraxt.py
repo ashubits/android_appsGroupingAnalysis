78f791f23324225319ef95b07ae56cf85d291cd6
Utkarsh Vaish <utkarshvaish9@gmail.com>
2016-11-22 12:15:15 +0530

    arguments added.

--- smali_extraxt.py
+++ smali_extraxt.py
@@ -1,21 +1,28 @@
 import fnmatch
 import os
-folderLoc = '/home/utkarsh/Documents/android'
-directory=[]
+import argparse
+parser = argparse.ArgumentParser(description='Output several text files of smali files combined, corrosponding to each apk')
+parser.add_argument('-d','--directory', help='Location of Directory(folder) to scan for apk files', required=True)
+parser.add_argument('-o','--txtfile', help='Location of the text file', required=True)
+args = vars(parser.parse_args())
+
+folderLoc = args['directory']
+directorys=[]
 
 for f_name in os.listdir(folderLoc):
-    directory.append(f_name)
+    directorys.append(f_name)
 
-for folders in directory:
+for folders in directorys:
 	absoluteLoc  = os.path.join(folderLoc,folders)
 	matches = []
 	for root, dirnames, filenames in os.walk(absoluteLoc):
 	    for filename in fnmatch.filter(filenames, '*.smali'):
 	        matches.append(os.path.join(root, filename))
-	textFileLoc = "/home/utkarsh/Documents/textFileApk/" + folders +".txt"
+	textFileLoc = args['txtfile'] + folders +".txt"
 	with open(textFileLoc, 'w') as outfile:
 	    for fname in matches:
 	        with open(fname) as infile:
 	            for line in infile:
 	                outfile.write(line)
+
 

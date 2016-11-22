47fb26ec0f8700dd7de5dc257c01d5e5a1d3a49c
Utkarsh Vaish <utkarshvaish9@gmail.com>
2016-11-22 22:49:26 +0530

    Update smali_extraxt.py

--- smali_extraxt.py
+++ smali_extraxt.py
@@ -1,13 +1,35 @@
 import fnmatch
 import os
 import argparse
+import shutil
 parser = argparse.ArgumentParser(description='Output several text files of smali files combined, corrosponding to each apk')
 parser.add_argument('-d','--directory', help='Location of Directory(folder) to scan for apk files', required=True)
 parser.add_argument('-o','--txtfile', help='Location of the text file', required=True)
 args = vars(parser.parse_args())
-
 folderLoc = args['directory']
+txtFileName = args['txtfile']
 directorys=[]
+if not (os.path.isdir(txtFileName + "/CALENDER")):
+	os.mkdir(txtFileName + "/CALENDER",0711)
+if not (os.path.isdir(txtFileName + "/CAMERA")):
+	os.mkdir(txtFileName + "/CAMERA",0711)
+if not (os.path.isdir(txtFileName + "/PHONE")):
+	os.mkdir(txtFileName + "/PHONE",0711)
+if not (os.path.isdir(txtFileName + "/SENSOR")):
+	os.mkdir(txtFileName + "/SENSOR",0711)
+if not (os.path.isdir(txtFileName + "/SMS")):
+	os.mkdir(txtFileName + "/SMS",0711)
+if not (os.path.isdir(txtFileName + "/CONTACTS")):
+	os.mkdir(txtFileName + "/CONTACTS",0711)
+if not (os.path.isdir(txtFileName + "/MICROPHONE")):
+	os.mkdir(txtFileName + "/MICROPHONE",0711)
+if not (os.path.isdir(txtFileName + "/LOCATION")):
+	os.mkdir(txtFileName + "/LOCATION",0711)
+if not (os.path.isdir(txtFileName + "/OTHERS")):
+	os.mkdir(txtFileName + "/OTHERS",0711)
+if not (os.path.isdir(txtFileName + "/STORAGE")):
+	os.mkdir(txtFileName + "/STORAGE",0711)
+
 
 for f_name in os.listdir(folderLoc):
     directorys.append(f_name)
@@ -18,7 +40,8 @@ for folders in directorys:
 	for root, dirnames, filenames in os.walk(absoluteLoc):
 	    for filename in fnmatch.filter(filenames, '*.smali'):
 	        matches.append(os.path.join(root, filename))
-	textFileLoc = args['txtfile'] + folders +".txt"
+
+	textFileLoc =folders +".txt"
 	with open(textFileLoc, 'w') as outfile:
 	    for fname in matches:
 	        with open(fname) as infile:
@@ -26,3 +49,5 @@ for folders in directorys:
 	                outfile.write(line)
 
 
+	shutil.move(textFileLoc,txtFileName + "/" +os.path.basename(args['directory']))
+	

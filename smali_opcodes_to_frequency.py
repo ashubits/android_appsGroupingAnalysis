bc4f32e72d66834a2454bd117adc85553e8a32db
Utkarsh Vaish <utkarshvaish9@gmail.com>
2016-11-28 15:14:18 +0530

    changed accordingly

--- smali_opcodes_to_frequency.py
+++ smali_opcodes_to_frequency.py
@@ -2,6 +2,13 @@
 from __future__ import print_function
 import os
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Output csv file with frequency of each opcode ')
+parser.add_argument('-d','--directory', help='Location of Directory(folder) to scan for smali text files', required=True)
+args = vars(parser.parse_args())
+
+
 
 f1 = open('opcodeList.txt', 'r')
 opcodes=[]
@@ -9,16 +16,21 @@ for eachLine in f1:
 	opcodes.extend(eachLine.split()) 			#adding all opcodes in an array
 f1.close()
 
-folderLoc= '/home/utkarsh/Documents/textFileApk'
+folderLoc= args['directory']
+# outputLoc  = args['csvfile']
 files=[]
 for filer in os.listdir(folderLoc):
     if filer.endswith(".txt"):
     	files.append(filer)
     
-orig_stdout = sys.stdout
-f = file('opcodes_count.csv', 'w')
-sys.stdout = f
-
+outputFile  = 'freq.csv'
+outfile= open(outputFile, 'w')
+outfile.write(' ')
+outfile.write(',')
+for codes in opcodes:
+	outfile.write(codes)
+	outfile.write(',')
+outfile.write('\n')
 for filer in files:
 	f1 = open(os.path.join(folderLoc, filer), 'r')
 	number=[0]*257
@@ -29,17 +41,18 @@ for filer in files:
 			if not(a.startswith('#')): 		#detecting comment or a function
 				if a in opcodes:								
 					number[opcodes.index(a)]+=1					#increase freq if a is in opcodes
-					
-
 	
+	outfile.write(filer)
+	
+	for x in range (0,256):
+		outfile.write(',')
+		outfile.write('%d'%number[x])
+		
+	outfile.write('\n')
 	f1.close()
-for x in range(0,256):
-	print(opcodes[x],end=',')
-	print(number[x],end='')
-	print()
+		
+	
+outfile.close()
 
-
-sys.stdout = orig_stdout
-sys.stdout.close()
 
 # print "Done! Saved to file",out_file

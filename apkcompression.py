36e23294610eea9a306ade75bc3eb9863c7045e8
Utkarsh Vaish <utkarshvaish9@gmail.com>
2017-03-23 14:43:21 +0530

    added comments

--- apkcompression.py
+++ apkcompression.py
@@ -1,8 +1,8 @@
 #!/usr/bin python2
-
+#paste this code where you want your uncompressed apk
 import os,argparse,sys,shutil
 
-parser = argparse.ArgumentParser(description='input location')
+parser = argparse.ArgumentParser(description='input location') #location where apks are present
 parser.add_argument('-d','--directory', help='input location', required=True)
 
 args = vars(parser.parse_args())
@@ -14,7 +14,7 @@ if not os.path.exists(folderLoc):
 	sys.exit()
 
 orig_stdout = sys.stdout
-f = file('out.txt', 'w')
+f = file('out.txt', 'w') #this file will have a value 0 if there is no error, else it will have a value of 256 for that apk
 sys.stdout = f
 files=[]
 for file in os.listdir(folderLoc):

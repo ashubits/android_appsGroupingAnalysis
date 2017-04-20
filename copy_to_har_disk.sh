295f3109605f00f9338f0ba7fcbb06f652fc7435
Utkarsh Vaish <utkarshvaish9@gmail.com>
2017-04-20 21:49:16 +0530

    Update copy_to_har_disk.sh

--- copy_to_har_disk.sh
+++ copy_to_har_disk.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-cd /home/user/Downloads/sample_apk
-for folder in */;
+
+#from external to internal
+
+cp -R /media/user/utkarsh/all_apk /home/user/Downloads/sample_apk #location where .apk files are present to location in internal memory where we want to copy
+#apktool
+cd /home/user/Downloads/faltu #location where we want decompressed apks
+for file in /home/user/Downloads/sample_apk/*.apk;  
 do
-	NUMBER=$(echo $folder | tr -dc '0-9')
-	echo $NUMBER
-	cd /media/user/utkarsh
-	mkdir $NUMBER
-	cp /home/user/Downloads/sample_apk/"$NUMBER"/AndroidManifest.xml /media/user/utkarsh/"$NUMBER"
-	cp -r /home/user/Downloads/sample_apk/"$NUMBER"/smali /media/user/utkarsh/"$NUMBER"
+	
+	apktool d $file
 done
+#grouping
+cd /home/user/Downloads/all_codes #location where android_permission code is present
+python android_permissions.py -d /home/user/Downloads/faltu -o /home/user/Downloads/sample_category #instructions on github
+#to generate frequency csv
+python smali_opcodes_to_frequency.py -d /home/user/Downloads/sample_category/CALENDER -o /home/user/Downloads/sample_category/CALENDER 
+#number of apks in each category
+chmod +x x.sh
+./x.sh
+
+#internal to external
+cp -R /home/user/Downloads/sample_category /media/user/utkarsh
+
+ 
+#done

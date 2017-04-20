3eb1f4d1263914d27bd3bb22b97824f252505816
Utkarsh Vaish <utkarshvaish9@gmail.com>
2017-04-20 23:03:07 +0530

    Update copy_to_har_disk.sh

--- copy_to_har_disk.sh
+++ copy_to_har_disk.sh
@@ -2,25 +2,37 @@
 
 #from external to internal
 
-cp -R /media/user/utkarsh/all_apk /home/user/Downloads/sample_apk #location where .apk files are present to location in internal memory where we want to copy
+cp -R /media/user/utkarsh/all_apk/ /home/user/Downloads/sample_apk #location where .apk files are present to location in internal memory where we want to copy
+
 #apktool
-cd /home/user/Downloads/faltu #location where we want decompressed apks
+cd /home/user/Downloads/sample_unzipped #location where we want decompressed apks
 for file in /home/user/Downloads/sample_apk/*.apk;  
 do
 	
 	apktool d $file
 done
+#transferring smali and manifest file to external disk
+cd /home/user/Downloads/sample_unzipped
+mkdir /media/user/utkarsh/unzipped
+for folder in */;
+ do
+ 	NUMBER=$(echo $folder | tr -dc '0-9')
+ 	cd /media/user/utkarsh/unzipped/ #location in harddisk where we want manifest file and smali file
+	mkdir $NUMBER
+ 	cp /home/user/Downloads/sample_apk/"$NUMBER"/AndroidManifest.xml /media/user/utkarsh/unzipped/"$NUMBER"
+ 	cp -R /home/user/Downloads/sample_apk/"$NUMBER"/smali /media/user/utkarsh/unzipped/"$NUMBER"
+ done
+
 #grouping
 cd /home/user/Downloads/all_codes #location where android_permission code is present
-python android_permissions.py -d /home/user/Downloads/faltu -o /home/user/Downloads/sample_category #instructions on github
-#to generate frequency csv
-python smali_opcodes_to_frequency.py -d /home/user/Downloads/sample_category/CALENDER -o /home/user/Downloads/sample_category/CALENDER 
-#number of apks in each category
-chmod +x x.sh
-./x.sh
+python android_permissions.py -d /media/user/utkarsh/unzipped -o /media/user/utkarsh/categorised #instructions on github
 
-#internal to external
-cp -R /home/user/Downloads/sample_category /media/user/utkarsh
+#to generate frequency csv
+python smali_opcodes_to_frequency.py -d /media/user/utkarsh/categorised/CALENDER -o /media/user/utkarsh/categorised/CALENDER #instructions on github
+
+#number of apks in each category
+chmod +x x.sh #x.sh is hardcoded so we need to change locations.
+./x.sh
 
  
 #done
